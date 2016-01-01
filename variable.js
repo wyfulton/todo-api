f89355ceefaeb34cad0148d32561314b2734b775
William Fulton <wyfulton@gmail.com>
2015-12-31 18:52:43 -0700

    Add variable.js

---
+++ variable.js
@@ -0,0 +1,32 @@
+var person = {
+	name: "Will",
+	age: 21
+}
+
+function updatePerson (obj) {
+	// obj = {
+	// 	name: "Will",
+	// 	age: 29
+	// };
+
+	obj.age = 29;
+}
+
+var array = [12, 90];
+
+function updateArray (arr) {
+	arr = [12, 43]
+}
+
+function updateArray2 (arr) {
+	arr.push(55);
+}
+
+updateArray(array);
+
+console.log(array)
+
+updateArray2(array);
+
+console.log(array)
+
