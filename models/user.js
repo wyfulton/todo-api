885b6ec1ee85cb8b1945fe3e8e83412ade61ee90
William Fulton <wyfulton@gmail.com>
2016-01-06 13:20:23 -0700

    add token generated by authentication

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
-var _ = require('underscore')
+var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -69,6 +71,24 @@ module.exports = function(sequelize, DataTypes) {
 				var json = this.toJSON();
 				return _.pick(json, "id", "email", "createdAt", "updatedAt");
 			},
+			generateToken: function (type) {
+				if (!_.isString(type)) {
+					return	undefined
+				}
+
+				try {
+					var stringData = JSON.stringify({id: this.get('id'), type: type});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#').toString();				
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwert');
+
+					return token;
+				} catch (e)	{
+					console.error(e);
+					return undefined;
+				}
+			}
 		}
 	});
 	
--- server.js
+++ server.js
@@ -145,16 +145,21 @@ app.post('/users', function(req, res) {
 app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password')
 
-	db.user.authenticate(body).then(function (user) {
-		res.json(user.toPublicJSON());
-	}, function () {
-		res.status(401).send();
-	});
+	db.user.authenticate(body)
+		.then(function (user) {
+			var token = user.generateToken('authentication');
 
-
+			if (token) {
+				res.header('Auth', token).json(user.toPublicJSON());
+			} else {
+				res.status(401).send();
+			};
+		}, function () {
+			res.status(401).send();
+		});
 })
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port' + PORT + '!');
 	});
